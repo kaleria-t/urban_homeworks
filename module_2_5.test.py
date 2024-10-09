fdba3bfe4228d3361cc4998da7142b883ac0b725
kaleria-t <kaleriatishina@gmail.com>
2024-10-10 00:36:13 +0300

    Add files via upload
    Функции в Python.Функция с параметром

--- module_2_5.test.py
+++ module_2_5.test.py
@@ -7,6 +7,9 @@ def get_matrix (n,m,value):
         for j in range (m):
             matrix[i].append(value)
     return matrix
-matrix = get_matrix(3,5,1)
-print(*matrix)
-
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)

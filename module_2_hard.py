a3ede48de6e1071d1b0eb01950f31aae53550a0d
kaleria-t <kaleriatishina@gmail.com>
2024-10-10 03:37:55 +0300

    Add files via upload
    Дополнительное практическое задание по модулю: "Основные операторы"

---
+++ module_2_hard.py
@@ -0,0 +1,11 @@
+import random
+nums = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+first_num = random.choice(nums)
+list_nums = []
+print (first_num)
+for i in range(1, first_num):
+    for k in range(i+1, first_num):
+        if first_num % (i + k) == 0:
+            list_nums.append(i)
+            list_nums.append(k)
+print(list_nums)

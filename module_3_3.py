f2a874d82499bf450ec6c6661cbf0d8b0891ab7e
kaleria-t <kaleriatishina@gmail.com>
2024-10-14 00:51:32 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -6,7 +6,8 @@ print_params(c = [1,2,3])
 
 values_list = ['cat', 71, [4,14]]
 values_dict = {'a': 32, 'b': 'tekst', 'c': [-7, 15]}
-print_params(*values_list, **values_dict)
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = [59, 'sun']
 print_params(*values_list_2, 42) # работает

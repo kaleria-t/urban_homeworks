3dbe4ee19f4f98c6744f71a99908794af1ce3202
kaleria-t <kaleriatishina@gmail.com>
2024-10-29 13:20:16 +0300

    Add files via upload

---
+++ module_4.2.py
@@ -0,0 +1,6 @@
+def test_function():
+    def inner_function():
+         return ("Я в области видимости функции test_function")
+    return inner_function()
+print (test_function())
+#print (inner_function())

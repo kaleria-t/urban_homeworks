c6eef9cf1af8e075514c3cc50978def04027d34b
kaleria-t <kaleriatishina@gmail.com>
2024-11-03 19:35:54 +0300

    Add files via upload
    Специальные методы классов

--- module_5_1.py
+++ module_5_1.py
@@ -6,13 +6,20 @@ class House:
         if new_floor > self.number_of_floors or new_floor < 1:
             print("Такого этажа не существует")
         else:
-            floors = []
             for i in range(1, new_floor+1):
-                floors.append(i)
                 print(i)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return str(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
 
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))

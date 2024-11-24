4b7b604fa805686686115d287992a4a0542bdb08
kaleria-t <kaleriatishina@gmail.com>
2024-11-24 16:22:24 +0300

    Add files via upload
    Позиционирование в файле

--- module_7_2.py
+++ module_7_2.py
@@ -1,6 +1,3 @@
-from _collections_abc import str_iterator
-
-
 def custom_write(file_name, strings):
     file = open(file_name, 'a', encoding='utf-8')
     string_positions = {}
@@ -8,7 +5,7 @@ def custom_write(file_name, strings):
     for i in strings:
         n += 1
         first_str_byte = file.tell()
-        file.write(i)
+        file.write(i+'\n')
         tuple_ = (n, first_str_byte)
         string_positions[tuple_] = i
     file.close()

96d72bc03146806e8a706a7f091c97d72f01935d
kaleria-t <kaleriatishina@gmail.com>
2024-12-09 05:04:28 +0300

    Add files via upload
    Создание функций на лету

---
+++ module_9_4.py
@@ -0,0 +1,24 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x==y, first, second)))
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for a in data_set:
+                file.write(str(a) + '\n')
+    return write_everything
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+    def __call__(self):
+        from random import choice
+        random_word = choice(self.words)
+        return random_word
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())

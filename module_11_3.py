fa333cc379aa97ab8417f51658b3161171fedd2e
kaleria-t <kaleriatishina@gmail.com>
2025-01-08 22:40:50 +0300

    module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -7,16 +7,13 @@ def introspection_info(obj):
     type_obj = type(obj)
 
     obj_atr = dir(obj)
-    # attr_list = []
-    # for attr_name in dir(obj):
-    #         attr_list.append(attr_name)
-    #         print(attr_name)
 
     obj_methods = (method for method in dir(obj) if callable(method))
 
     module = inspect.getmodule(introspection_info)
 
     return (f'Объект: {obj}, тип: {type_obj}, атрибуты: {obj_atr}, методы: {obj_methods}, из модуля: {module}')
+    
 print(number_info := introspection_info(42))
 
 
